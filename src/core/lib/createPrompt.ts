710d9c71c024b4d8b894586d348dd1ffd7d88d51
Gerson Mayer <g.mayerduran@gmail.com>
2023-09-08 02:57:59 -0500

    removed values from prompt

--- src/core/lib/createPrompt.ts
+++ src/core/lib/createPrompt.ts
@@ -2,9 +2,13 @@ import { ParsedThing } from "core/models/models"
 import { cleanText } from "core/lib/textUtils";
 
 export const createPrompt = (game: ParsedThing) => {
-    const newPrompt = `Boardgame: ${game.name}. Description: ${game.description}. Year Published: ${game.yearpublished}
-    Designers: ${game.boardgamedesigner.join(", ")}. Mechanics: ${game.boardgamemechanic.join(", ")}. Categories: ${game.boardgamecategory.join(", ")}.
+
+    const newPrompt = ` 
+     Mechanics: ${game.boardgamemechanic.join(", ")}. Categories: ${game.boardgamecategory.join(", ")}.
     Family: ${game.boardgamefamily.join(", ")}.`
     const cleanPrompt=  cleanText(newPrompt)
+    cleanPrompt.replace(game.name, "")
+    cleanPrompt.replace(game.name.toLowerCase(), "")
+    cleanPrompt.replace(game.name.toUpperCase(), "")
     return cleanPrompt
 }
