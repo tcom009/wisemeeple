0a85ecac9d524644c3ea6fd977c9e3bc4bea744c
Gerson Mayer <g.mayerduran@gmail.com>
2023-09-12 23:50:45 -0500

    added useClickOutside hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import BGGUserForm from "@/core/components/forms/BGGUserForm";
+import SearchForm from "@/core/components/forms/SearchForm";
 import { Container } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <Container size={{lg:"2", md:"2", sm:"1", xs:"1"}} mt={"9"}>
-      <BGGUserForm />
+      <SearchForm />
     </Container>
   );
 }
--- src/core/components/forms/SearchForm.tsx
+++ src/core/components/forms/SearchForm.tsx
@@ -1,21 +1,21 @@
 "use client";
 import {
   Flex,
-  Grid,
   Button,
-  TextFieldInput,
   TextField,
   Text,
   Heading,
   Card,
   Separator,
+  IconButton,
 } from "@radix-ui/themes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSearch } from "react-use-search";
 import games from "@/core/data/games.json";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-
+import { MagnifyingGlassIcon, Cross2Icon } from "@radix-ui/react-icons";
+import { useClickOutside } from "@/core/hooks/useClickOutside";
+import { capitalize } from "@/core/lib/textUtils";
 const predicate = (
   game: {
     name: string;
@@ -30,19 +30,26 @@ export default function BGGUserForm() {
       name: game.name.toLowerCase(),
     };
   });
-  const [suggestions, setSuggestions] = useState<string>("");
   const [error, setError] = useState(false);
   const [filteredGames, query, handleChange, setQuery] = useSearch(
     data,
     predicate,
     { debounce: 200 }
   );
-
-  const capitalize = (str: string) =>
-    `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+  const ref = useClickOutside(() => setQuery(""));
+  const onSubmit = () => {
+    if (!query) {
+      setError(true);
+    }
+  };
+  useEffect(() => {
+    if (query.length !== 0) {
+      setError(false);
+    }
+  },[query])
 
   return (
-    <Flex gap="3" direction={"column"}>
+    <Flex direction={"column"}>
       <Flex justify={"center"} direction={"column"} gap={"3"}>
         <Heading
           align={"center"}
@@ -62,7 +69,7 @@ export default function BGGUserForm() {
           First, let&apos;s search a boardgame:
         </Text>
       </Flex>
-      <TextField.Root size={"3"}>
+      <TextField.Root size={"3"} mt={"5"}>
         <TextField.Slot>
           <MagnifyingGlassIcon />
         </TextField.Slot>
@@ -74,23 +81,32 @@ export default function BGGUserForm() {
           onChange={handleChange}
           required
         />
+        {query.length !== 0 && (
+          <TextField.Slot pr="3">
+            <IconButton size="2" variant="ghost" onClick={() => setQuery("")}>
+              <Cross2Icon height="16" width="16" />
+            </IconButton>
+          </TextField.Slot>
+        )}
       </TextField.Root>
-
       {filteredGames.length !== 0 && (
-        <Card>
+        <Card ref={ref}>
           <Flex direction={"column"}>
             {filteredGames
               .slice(0, 9)
               .map((game: { id: string; name: string }, index: number) => (
                 <Link
-                href={`/generate/${game.id}`}
-                key={game.id}
-                className="no-underline white-link"
+                  href={`/generate/${game.id}`}
+                  key={game.id}
+                  className="no-underline white-link"
                 >
                   {index !== 0 && (
                     <Separator orientation="horizontal" size={"4"} mb={"4"} />
                   )}
-                  <Text key={game.id} weight={"bold"} mb={"2"} as={"div"}> {capitalize(game.name)}</Text>
+                  <Text key={game.id} weight={"bold"} mb={"2"} as={"div"}>
+                    {" "}
+                    {capitalize(game.name)}
+                  </Text>
                 </Link>
               ))}
           </Flex>
@@ -98,17 +114,18 @@ export default function BGGUserForm() {
       )}
 
       {error && (
-        <Text as={"span"} color={"crimson"} size={"2"}>
-          Please enter a username or a boardgame name
+        <Text as={"span"} color={"crimson"} size={"2"} mt={"2"}>
+          Please enter a boardgame name
         </Text>
       )}
       <Flex direction={"row"} gap={"3"} align={"center"} justify={"center"}>
-        { filteredGames.length === 0 &&
-        <Link href={query && `/search/${query}`} className="no-underline">
-          <Button size={"3"} mt={"3"} mb={"5"}>
-            Search Game
-          </Button>
-        </Link>}
+        {filteredGames.length === 0 && (
+          <Link href={query && `/search/${query}`} className="no-underline">
+            <Button size={"3"} mt={"5"} onClick={onSubmit}>
+              Search Game
+            </Button>
+          </Link>
+        )}
       </Flex>
     </Flex>
   );
