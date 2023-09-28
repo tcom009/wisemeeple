789769631e013924984b54650edbde89894d0feb
Gerson Mayer <g.mayerduran@gmail.com>
2023-09-28 14:58:40 -0500

    forcing dynamic render

--- src/app/unauthenticated/page.tsx
+++ src/app/unauthenticated/page.tsx
@@ -1,6 +1,9 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation" 
+
+export const dynamic = 'force-dynamic'
+
 export default async function UnauthenticatedPage() {
   const supabase = createServerComponentClient({ cookies })
   const {data: {session }} = await supabase.auth.getSession()
