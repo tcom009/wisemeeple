f8f92f864a7ea99863dee8ec57e57b8d117f4794
Gerson Mayer <g.mayerduran@gmail.com>
2023-09-28 13:26:21 -0500

    Feat: added middleware and route handler for using cookies in supabase auth Feat: initialized supabase local dev in project Feat: added unauthorized page

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,5 +1,21 @@
 import { Flex, Grid, Text, Container } from "@radix-ui/themes";
 import SellForm from "app/sell/SellForm";
-export default function SellPage() {
-  return <Container size={{ lg: "3", md: "3", sm: "1", xs: "1" }}> <SellForm/></Container>;
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+export default async function SellPage() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect("/unauthenticated");
+  }
+  return (
+    <Container size={{ lg: "3", md: "3", sm: "1", xs: "1" }}>
+      {" "}
+      <SellForm />
+    </Container>
+  );
 }
--- src/core/lib/getVectors.ts
+++ src/core/lib/getVectors.ts
@@ -1,6 +1,6 @@
 import { OpenAI } from "openai";
 
-const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const embeddingGenerator = async (prompt: string) => {
     const response = await openai.embeddings.create({
