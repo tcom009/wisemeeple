b0593d72ef7d61f5ddba9cf50e954717f0140a7b
Gerson Mayer <g.mayerduran@gmail.com>
2023-09-28 14:46:24 -0500

    removed unused instances

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,6 @@
 import LoginForm from "./LoginForm"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import  { cookies } from "next/headers"
 export default function LoginPage(){
-    const supabase = createServerComponentClient({ cookies })
+    //const supabase = createServerComponentClient({ cookies })
     
     return <div><LoginForm/></div>
 }
