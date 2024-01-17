e6cf9106d81d79f932d3a073cd0b814f2dd43cfa
gersonmayer <g.mayerduran@gmail.com>
2024-01-17 12:12:56 -0500

    Fix: added behaviour onKeypress for searchbox accesibility

--- src/app/SearchForm.tsx
+++ src/app/SearchForm.tsx
@@ -45,7 +45,8 @@ export default function SearchForm() {
     router.push(`/generate/${id}`);
   };
   const handleKeyPress = (event: any) => {
-    if (event.key === "Enter" && !isOpen) {
+    if (event.key === "Enter" && filteredGames.length === 0) {
+      console.log('hola');
       onSubmit();
     }
     if (event.key === "Escape") {
