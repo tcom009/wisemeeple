92f758a93408b279a364bc1241ea299273ddff77
gersonmayer <g.mayerduran@gmail.com>
2024-02-24 10:45:51 -0500

    fixed missing package

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,20 +1,12 @@
 import { Flex, Grid, Text, Container } from "@radix-ui/themes";
 import SellForm from "app/sell/SellForm";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
 export const dynamic = 'force-dynamic'
 
 export default async function SellPage() {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session) {
-    redirect("/unauthenticated");
-  }
+  
   return (
     <Container size={{ lg: "3", md: "3", sm: "1", xs: "1" }}>
       {" "}
